5223c03b1c85e715bec0a9ba99a53478fc9f0fda
Rafael-Rieder-Dias <rafael.rieder@edu.pucrs.br>
2025-08-04 19:40:06 -0300

    Update CodificadorPedro.java

--- Codificador/src/main/java/pacote/CodificadorPedro.java
+++ Codificador/src/main/java/pacote/CodificadorPedro.java
@@ -1,5 +1,39 @@
 package pacote;
 
-public class CodificadorPedro{
-    
+import java.time.LocalDate;
+
+public class CodificadorPedro implements Codificador{
+    public String getNome(){
+        return "Codificador Pedro";
+    }
+
+    public LocalDate getDataCriacao(){
+        return LocalDate.of(2025, 8, 04);
+    }
+
+    public int getNivelSeguranca(){
+        return 2;
+    }
+
+    public String codifica(String str) {
+        StringBuilder encoded = new StringBuilder();
+
+        
+
+        for (char c : str.toCharArray()) {
+            encoded.append((char) (c + 14));
+        }
+
+        return encoded.toString();
+    }
+
+    public String decodifica(String str) {
+        StringBuilder encoded = new StringBuilder();
+        
+        for (char c : str.toCharArray()) {
+            encoded.append((char) (c - 14));
+        }
+        
+        return encoded.toString();
+    }
 }
